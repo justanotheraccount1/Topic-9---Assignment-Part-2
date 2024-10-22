9506815f16fa216163b0fdbc88bc759c82ec1850
Sam Ahrens <339729139@GoTVDSB.ca>
2024-10-22 09:37:22 -0400

    Add project files.

---
+++ Topic 9 - Assignment Part 2/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Topic_9___Assignment_Part_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnTimes = new System.Windows.Forms.Button();
+            this.btnDivision = new System.Windows.Forms.Button();
+            this.lblPlus = new System.Windows.Forms.Label();
+            this.lblMinus = new System.Windows.Forms.Label();
+            this.lblTimes = new System.Windows.Forms.Label();
+            this.lblDivide = new System.Windows.Forms.Label();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(-1, -1);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(118, 109);
+            this.btnPlus.TabIndex = 0;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(-1, 111);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(118, 109);
+            this.btnMinus.TabIndex = 1;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnTimes
+            // 
+            this.btnTimes.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTimes.Location = new System.Drawing.Point(-1, 226);
+            this.btnTimes.Name = "btnTimes";
+            this.btnTimes.Size = new System.Drawing.Size(118, 109);
+            this.btnTimes.TabIndex = 2;
+            this.btnTimes.Text = "x";
+            this.btnTimes.UseVisualStyleBackColor = true;
+            this.btnTimes.Click += new System.EventHandler(this.btnTimes_Click);
+            // 
+            // btnDivision
+            // 
+            this.btnDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivision.Location = new System.Drawing.Point(-1, 341);
+            this.btnDivision.Name = "btnDivision";
+            this.btnDivision.Size = new System.Drawing.Size(118, 109);
+            this.btnDivision.TabIndex = 3;
+            this.btnDivision.Text = "/";
+            this.btnDivision.UseVisualStyleBackColor = true;
+            this.btnDivision.Click += new System.EventHandler(this.btnDivision_Click);
+            // 
+            // lblPlus
+            // 
+            this.lblPlus.AutoSize = true;
+            this.lblPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlus.Location = new System.Drawing.Point(123, 32);
+            this.lblPlus.Name = "lblPlus";
+            this.lblPlus.Size = new System.Drawing.Size(158, 55);
+            this.lblPlus.TabIndex = 4;
+            this.lblPlus.Text = "label1";
+            this.lblPlus.Visible = false;
+            // 
+            // lblMinus
+            // 
+            this.lblMinus.AutoSize = true;
+            this.lblMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinus.Location = new System.Drawing.Point(123, 144);
+            this.lblMinus.Name = "lblMinus";
+            this.lblMinus.Size = new System.Drawing.Size(158, 55);
+            this.lblMinus.TabIndex = 5;
+            this.lblMinus.Text = "label1";
+            this.lblMinus.Visible = false;
+            // 
+            // lblTimes
+            // 
+            this.lblTimes.AutoSize = true;
+            this.lblTimes.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTimes.Location = new System.Drawing.Point(123, 259);
+            this.lblTimes.Name = "lblTimes";
+            this.lblTimes.Size = new System.Drawing.Size(158, 55);
+            this.lblTimes.TabIndex = 6;
+            this.lblTimes.Text = "label2";
+            this.lblTimes.Visible = false;
+            // 
+            // lblDivide
+            // 
+            this.lblDivide.AutoSize = true;
+            this.lblDivide.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDivide.Location = new System.Drawing.Point(123, 374);
+            this.lblDivide.Name = "lblDivide";
+            this.lblDivide.Size = new System.Drawing.Size(158, 55);
+            this.lblDivide.TabIndex = 7;
+            this.lblDivide.Text = "label3";
+            this.lblDivide.Visible = false;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackColor = System.Drawing.Color.Black;
+            this.btnQuit.Font = new System.Drawing.Font("Stencil", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnQuit.ForeColor = System.Drawing.Color.White;
+            this.btnQuit.Location = new System.Drawing.Point(538, 343);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(285, 107);
+            this.btnQuit.TabIndex = 8;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 449);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.lblDivide);
+            this.Controls.Add(this.lblTimes);
+            this.Controls.Add(this.lblMinus);
+            this.Controls.Add(this.lblPlus);
+            this.Controls.Add(this.btnDivision);
+            this.Controls.Add(this.btnTimes);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnTimes;
+        private System.Windows.Forms.Button btnDivision;
+        private System.Windows.Forms.TextBox lblAnswer;
+        private System.Windows.Forms.Label lblPlus;
+        private System.Windows.Forms.Label lblMinus;
+        private System.Windows.Forms.Label lblTimes;
+        private System.Windows.Forms.Label lblDivide;
+        private System.Windows.Forms.Button btnQuit;
+    }
+}
+
---
+++ Topic 9 - Assignment Part 2/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Topic_9___Assignment_Part_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            lblPlus.Visible = true;
+            lblMinus.Visible = false;
+            lblTimes.Visible = false;
+            lblDivide.Visible = false;
+            lblPlus.Text = ("8 + 8 = " + (8 + 8));
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            lblMinus.Visible = true;
+            lblTimes.Visible = false;
+            lblPlus.Visible = false;
+            lblDivide.Visible = false;
+            lblMinus.Text = ("8 - 8 = " + (8 - 8));
+        }
+
+        private void btnTimes_Click(object sender, EventArgs e)
+        {
+            lblTimes.Visible = true;
+            lblMinus.Visible = false;
+            lblPlus.Visible = false;
+            lblDivide.Visible = false;
+            lblTimes.Text = ("8 x 8 = " + (8 * 8));
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            lblDivide.Visible = true;
+            lblMinus.Visible = false;
+            lblPlus.Visible = false;
+            lblTimes.Visible = false;
+            lblDivide.Text = ("8 / 8 = " + (8 / 8));
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
